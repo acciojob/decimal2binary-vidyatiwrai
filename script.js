665f40eae9322146da4697197ae86045e64a5de1
vidyatiwrai <127404171+vidyatiwrai@users.noreply.github.com>
2023-04-14 23:17:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function threeSum(arr) {
+function threeSum(string) {
 // write your code here
   
 // let decimal = 10;
 let redix = 2;
-let binary = arr.toString( redix );
+let binary = string.toString( redix );
 console.log(binary);
 }
 

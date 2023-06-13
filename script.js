989fbca3f181f375f00abc19f278b40b2789e5f1
Vidya Tiwari <127404171+vidyatiwrai@users.noreply.github.com>
2023-06-13 08:51:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-function decimalToBinary(num){
-       let bin = 0;
-    let rem, i = 1, step = 1;
-    while (x != 0) {
-        rem = x % 2;
-        x = parseInt(x / 2);
-        bin = bin + rem * i;
-        i = i * 10;
-    }
-    console.log(`Binary: ${bin}`);
+function decimalToBinary(decimal) {
+  let binary = '';
+  let quotient = decimal;
+
+  while (quotient !== 0) {
+    const remainder = quotient % 2;
+    binary = remainder + binary;
+    quotient = Math.floor(quotient / 2);
+  }
+
+  return binary;
 }
     
   
